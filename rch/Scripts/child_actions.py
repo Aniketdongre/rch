11fc6efc9f92440a3657e54694bb4b8498389340
Aniketdongre <dongreaniket2020@gmail.com>
2020-07-16 14:09:03 +0530

    modulation done

--- rch/Scripts/child_actions.py
+++ rch/Scripts/child_actions.py
@@ -1,40 +1,40 @@
 
-from rch_actions import *
+import rch_actions as ra
 from time import  sleep
 
 
 def Child():
     print("i am inside ch1")
-    from main import rchBot
+
 
     # method for  'SECTION-III(INDEX) Tracking of Children (CH-1)' page
 
-    driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_btnContinue"]').click()
+    ra.driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_btnContinue"]').click()
 
-    self.setData()
+    setData()
 
     # clicks on submit button
-    rchBot.driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_btnSave"]').click()
+    ra.driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_btnSave"]').click()
     sleep(3)
 
     # alert_handling
 
-    alertHandler()
+    ra.alertHandler()
 
     # sets child immunization options according to option selected
 
 
 
-def setOption(self):
+def setOption():
 
     sleep(20)
-    self.selected_dose = driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ddlImmunization"]').get_attribute('value')
+    selected_dose = ra.driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ddlImmunization"]').get_attribute('value')
 
-    print("selected value is ",self.selected_dose)
+    print("selected value is ",selected_dose)
 
-    if self.selected_dose =='23':
-        driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ChkBoxImu3"]').click()
-        driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ChkBoxImu5"]').click()
+    if selected_dose =='23':
+        ra.driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ChkBoxImu3"]').click()
+        ra.driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ChkBoxImu5"]').click()
         sleep(3)
     else:
         sleep(2)
@@ -43,7 +43,7 @@ def setOption(self):
 
 
 
-def setData(self):
+def setData():
 
 
     print("inside child track page and set data method")
@@ -53,8 +53,8 @@ def setData(self):
 
         # click on immunization selection
 
-    driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ddlImmunization"]').click()
+    ra.driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ddlImmunization"]').click()
 
-    self.setOption() #sets the immunization options according to selection
+    setOption() #sets the immunization options according to selection
 
-    rch_actions.setDate()
+    ra.setDate()
--- rch/Scripts/rch_actions.py
+++ rch/Scripts/rch_actions.py
@@ -1,15 +1,90 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
 from time import  sleep
+import child_actions as ca
+
+
+
+driver = ''
+
+
+def login(username, pw):
+    global driver
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.rch.nhm.gov.in/RCH/")
+    sleep(1)
+    # Auto login
+    driver.find_element_by_xpath('//*[@id="form1"]/header/div[2]/div[2]/div[3]').click()
+    sleep(1)
+
+    # selects state Maharastra
+    driver.find_element_by_xpath('//*[@id="ddlStateName"]/option[21]').click()
+
+    # sets usaername
+    driver.find_element_by_xpath('//*[@id="txtUserName"]') \
+        .send_keys(username)
+    sleep(1)
+
+    # sets Password
+    driver.find_element_by_xpath('//*[@id="txtPassword"]') \
+        .send_keys(pw)
+    sleep(10)
+    # self.captcha = self.driver.find_element_by_xpath('//*[@id="txtimgcode"]').get_attribute('value')
+
+    # clicks on login
+    driver.find_element_by_xpath('//*[@id="btnLogin"]').click()
+
+    driver.find_element_by_xpath('//*[@id="SMSMenu"]/span').click()
+
+    sleep(5)
+
+    driver.find_element_by_xpath('//*[@id="SingleMainContent_UserInfoBoxControl2_ddlSubCentre"]/option[5]').click()
+
+    sleep(3)
+    driver.find_element_by_xpath('//*[@id="SingleMainContent_UserInfoBoxControl2_ddlVillage"]/option[3]').click()
+    sleep(2)
+    driver.find_element_by_xpath('//*[@id="SingleMainContent_UserInfoBoxControl2_btnSave"]').click()
+    sleep(4)
+
+    print("location selected")
+    # home button click
+    driver.find_element_by_xpath('//*[@id="HomeMenu"]/span').click()
+
+    sleep(30)
+
+
+
+
+def get_page():
+    print("i am inside get_page")
+
+    sleep(3)
+
+    # store url of currently loaded page
+
+    page_url = driver.current_url
+
+    # url of 'SECTION-III(INDEX) Tracking of Children (CH-1)' page
+
+    urlCh1 = 'https://rch.nhm.gov.in/RCH/UI/ChildDataEntry.aspx?Id_Edit='
+
+    # compairing current page url with url of all pages by comapiring substrig method
+
+    if urlCh1 in page_url:
+        # calling corresponding page method
+        ca.Child()
+
+
 
 
     #method for explicit wait until element located by xpath, xpath is given to constructor
 
 def ex_wait(xpath):
-    from main import rchBot
     try:
-        element = WebDriverWait(rchBot.driver,30).until(EC.presence_of_element_located((By.XPATH,xpath))
+        element = WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,xpath))
         )
         return element
     finally:
@@ -21,13 +96,12 @@ def ex_wait(xpath):
 
 
 def setDate():
-    from main import rchBot
 
     sleep(4)
-    self.date = '20-05-2020'
+    date = '20-05-2020'
     print('inside set date')
 
-    rchBot.driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_txtImmu_Date"]').send_keys(self.date) #sets date in immunization date field
+    driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_txtImmu_Date"]').send_keys(date) #sets date in immunization date field
 
 
     #method to handle alerts according to alert message
@@ -35,36 +109,18 @@ def setDate():
 
 def alertHandler():
 
-    from main import rchBot
 
     #get Alert_text
 
-    alert_text = rchBot.driver.switch_to_alert().text
+    alert_text = driver.switch_to_alert().text
 
     print("Alert text is : ",alert_text)
 
-    self.success = 'Successfully'
+    success = 'Successfully'
     # checks if alert is of successfull
 
     if success in alert_text:
         #clicks on on alert
-        rchBot.driver.switch_to_alert().accept()
+        driver.switch_to_alert().accept()
 
 
-    #method to set Data Entry Data i.e Doses or ANC visits
-
-  #  def setData(self):
-
-
-    #    print("inside child track page and set data method")
-
-
-    #    sleep(2) #bas yuhi
-
-    #    # click on immunization selection
-
-    #    driver.find_element_by_xpath('//*[@id="SingleMainContent_DoubleMainContent_ddlImmunization"]').click()
-
-     #   self.setOption() #sets the immunization options according to selection
-
-     #   self.setDate()
